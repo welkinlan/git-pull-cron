b6cbedf59f0c2d9c642cee46e512ba8edb1f6494
Tian <welkinlan@gmail.com>
2018-01-24 04:01:13 +0000

    Add a new folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var CronJob = require('cron').CronJob;
 var exec = require('child_process').exec;
 
-new CronJob('1 * * * * *', function() {
+new CronJob('* * * * * *', function() {
   exec('cd /var/www/lanyujiance.com/html/site/; git pull', (err, stdout, stderr) => {
     if (err) {
       console.error(err)
@@ -23,5 +23,16 @@ new CronJob('1 * * * * *', function() {
     // the *entire* stdout and stderr (buffered)
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
-  });	
+  });
+  exec('cd /var/www/welkinlan.com/personal-wesite; git pull', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err)
+      // node couldn't execute the command
+      return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }, null, true, 'America/Los_Angeles');
