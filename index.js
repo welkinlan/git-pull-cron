f702a2669edcedbfdac1617e587b734d94be5d4a
Tian Lan <welkinlan@gmail.com>
2021-02-17 14:39:38 +0800

    updatede

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 var exec = require('child_process').exec;
 
 new CronJob('* * * * * *', function() {
-  exec('cd /var/www/lanyucorp.com/public_html/LanyuCorpWebsite; git pull', (err, stdout, stderr) => {
+  exec('cd /var/www/lanyucorp.com/public_html/LanyuCorpWebsite; git checkout master; git fetch origin; git merge origin/master;', (err, stdout, stderr) => {
     if (err) {
       console.error(err)
       // node couldn't execute the command
@@ -13,7 +13,7 @@ new CronJob('* * * * * *', function() {
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
   });  
-  exec('cd /var/www/lanyujiance.com/public_html/LaresTestingWebsite; git pull', (err, stdout, stderr) => {
+  exec('cd /var/www/lanyujiance.com/public_html/LaresTestingWebsite; git checkout master; git fetch origin; git merge origin/master;', (err, stdout, stderr) => {
     if (err) {
       console.error(err)
       // node couldn't execute the command
